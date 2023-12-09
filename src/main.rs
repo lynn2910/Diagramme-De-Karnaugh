23f125da2f3d7718883b73b1ae8035b0201f1bde
lynn <cedriccolinc@yahoo.com>
2023-12-09 11:49:27 +0100

    Ajout d'une interface dynamique
    - On peut maintenant donner un fichier en guise de source avec ce format:
    ```
    1001
    0110
    0110
    1001
    ```

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
+use std::fs;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::PathBuf;
+#[cfg(feature = "time_calc")]
 use std::time::Instant;
+use clap::Parser;
 
 mod printer;
 mod blocks;
@@ -45,24 +51,69 @@ fn generate_gray_code(n: usize) -> Vec<String> {
     gray_code
 }
 
+#[derive(Parser)]
+struct App {
+    #[clap(short, long)]
+    diagram: PathBuf
+}
+
+fn parse_table_from_file(path: &PathBuf) -> Vec<Vec<i32>> {
+    let f = fs::read_to_string(path).expect("Impossible d'ouvrir le fichier");
+
+    let mut diagram = Vec::new();
+    let mut columns = None;
+
+    for line in f.split('\n') {
+        let mut bools = Vec::new();
+        for c in line.chars() {
+            match c {
+                '0' => bools.push(0),
+                '1' => bools.push(1),
+                _ => panic!("Invalid input: the diagram can only have 0 and 1")
+            }
+        }
+
+        match columns {
+            None => columns = Some(bools.len()),
+            Some(c) if bools.len() != c => panic!("A row don't have the same length as the others"),
+            _ => {}
+        }
+
+        diagram.push(bools)
+    }
+
+    dbg!(&diagram);
+
+    diagram
+}
+
 fn main() {
-    let karnaugh_table: KarnaughDiagram = &[
-        &[0, 1, 1, 0],
-        &[1, 0, 0, 1],
-        &[1, 0, 0, 1],
-        &[0, 1, 1, 0]
-    ];
-    let number_of_vars = 4;
+    let app = App::parse();
+
+    // let karnaugh_diagram: KarnaughDiagram = &[
+    //     &[0, 1, 1, 0],
+    //     &[1, 0, 0, 1],
+    //     &[1, 0, 0, 1],
+    //     &[0, 1, 1, 0]
+    // ];
+    // let number_of_vars = 4;
+    let parsed_diagram = parse_table_from_file(&app.diagram);
+    let karnaugh_diagram = parsed_diagram.iter()
+        .map(|u| u.as_slice())
+        .collect::<Vec<&[i32]>>();
+    let karnaugh_diagram = karnaugh_diagram.as_slice();
+
+    let number_of_vars = karnaugh_diagram.len();
     let gray_code = generate_gray_code(number_of_vars / 2);
 
     #[cfg(feature = "draw_table")]
-    printer::print_table(&karnaugh_table, number_of_vars, &gray_code);
+    printer::print_table(&karnaugh_diagram, number_of_vars, &gray_code);
 
     #[cfg(feature = "verbose")]
     println!("Calcule en cours des blocs...");
     #[cfg(feature = "time_calc")]
     let n = Instant::now();
-    let blocks = blocks::find_blocks(&karnaugh_table);
+    let blocks = blocks::find_blocks(&karnaugh_diagram);
     #[cfg(feature = "time_calc")]
     println!("Calculs des blocs (of 0 & 1) effectué en {}s", n.elapsed().as_secs_f32());
 
